49b12e6dc374a14ebfb864f89ee63930a6b6dd3c
Sabareeswaran B <sabareeswaran2510@gmail.com>
2022-03-28 14:57:47 +0530

    dbcontext added in program.cs

--- Models/GasB360Context.cs
+++ Models/GasB360Context.cs
@@ -31,7 +31,6 @@ namespace GasB360_server.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Server=tcp:gasb360.database.windows.net,1433;Initial Catalog=GasB360;Persist Security Info=False;User ID=admin@gasb360@gasb360;Password=Kovai.co;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
