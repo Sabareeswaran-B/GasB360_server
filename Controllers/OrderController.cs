8141a652f73f1e769c26daaeed5de0f1c7d794fd
VISHNUPRASHOB <vishnuprashob07@gmail.com>
2022-03-30 08:57:58 +0530

    order update

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -33,7 +33,7 @@ namespace GasB360_server.Controllers
            catch (System.Exception ex)
            {
                Console.WriteLine(ex);
-               return BadRequest(new{status="Order GET request Failed",message = ex.Message});
+               return BadRequest(new{status="Failed",message = ex.Message});
            }
         }
 
