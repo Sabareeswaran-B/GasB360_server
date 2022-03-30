0de211debf6a48b3f639d4d57867a39de8be1242
Sabareeswaran B <sabareeswaran2510@gmail.com>
2022-03-30 18:00:13 +0530

    type naming convention

--- Controllers/TypeController.cs
+++ Controllers/TypeController.cs
@@ -25,153 +25,133 @@ namespace GasB360_server.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAllTypes()
         {
-              try
-           {
-              var type = await _context.TblTypes.ToListAsync();
-              return Ok(
-                    new
-                    {
-                        status = "success",
-                        message = "Get all type successful.",
-                        data = type
-                    }
+            try
+            {
+                var type = await _context.TblTypes.ToListAsync();
+                return Ok(
+                    new { status = "success", message = "Get all type successful.", data = type }
                 );
-           }
-           catch (System.Exception ex)
-           {
-               
-               return BadRequest(new{status="Failed",message = ex.Message});
-           }
+            }
+            catch (System.Exception ex)
+            {
+                Sentry.SentrySdk.CaptureException(ex);
+                return BadRequest(new { status = "Failed", message = ex.Message });
+            }
         }
 
         // GET: api/Type/5
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetTypeById(Guid id)
+        [HttpGet("{typeId}")]
+        public async Task<IActionResult> GetTypeById(Guid typeId)
         {
-          
-               try
-           {
-               var tblType = await _context.TblTypes.FindAsync(id);
-
-            if (tblType == null)
+            try
             {
-                return NotFound();
-            }
+                var tblType = await _context.TblTypes.FindAsync(typeId);
 
-            return Ok(
+                if (tblType == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(
                     new
                     {
                         status = "success",
                         message = "Get type by id successful.",
-                        data =tblType
-                    }
-                );
-           }
-           catch (System.Exception ex)
-           {
-               
-               return BadRequest(new{status="Failed",message = ex.Message});
-           }
-        }
-
-        // PUT: api/Type/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> updateType(Guid id, TblType tblType)
-        {
-            if (id != tblType.TypeId)
-            {
-                return BadRequest();
-            }
-
-
-            try
-            {
-            _context.Entry(tblType).State = EntityState.Modified;
-                await _context.SaveChangesAsync();
-                 var type =await _context.TblTypes.FindAsync(id);
-                 return Ok(
-                    new
-                    {
-                        status = "success",
-                        message = "Update type successful.",
-                        data = type
+                        data = tblType
                     }
                 );
             }
             catch (System.Exception ex)
             {
-                if (!TblTypeExists(id))
+                Sentry.SentrySdk.CaptureException(ex);
+                return BadRequest(new { status = "Failed", message = ex.Message });
+            }
+        }
+
+        // PUT: api/Type/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{typeId}")]
+        public async Task<IActionResult> updateType(Guid typeId, TblType tblType)
+        {
+            if (typeId != tblType.TypeId)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                _context.Entry(tblType).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                var type = await _context.TblTypes.FindAsync(typeId);
+                return Ok(
+                    new { status = "success", message = "Update type successful.", data = type }
+                );
+            }
+            catch (System.Exception ex)
+            {
+                if (!IsTypeExists(typeId))
                 {
                     return NotFound();
                 }
                 else
                 {
-                     return BadRequest(new{status="Failed",message = ex.Message});
+                    Sentry.SentrySdk.CaptureException(ex);
+                    return BadRequest(new { status = "Failed", message = ex.Message });
                 }
             }
 
-           
+
         }
 
         // POST: api/Type
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<IActionResult>AddType(TblType tblType)
+        public async Task<IActionResult> AddType(TblType tblType)
         {
-               try
-           {
-            _context.TblTypes.Add(tblType);
-            await _context.SaveChangesAsync();
-            return CreatedAtAction("GetTypeById",
-             new { id = tblType.TypeId },
-                new
-                    {
-                        status = "success",
-                        message = "Add new type successful.",
-                        data = tblType
-                    }
-              );
-           }
-           catch (System.Exception ex)
-           {   
-            return BadRequest(new{status="Failed",message = ex.Message});
-           }
+            try
+            {
+                _context.TblTypes.Add(tblType);
+                await _context.SaveChangesAsync();
+                return CreatedAtAction(
+                    "GetTypeById",
+                    new { typeId = tblType.TypeId },
+                    new { status = "success", message = "Add new type successful.", data = tblType }
+                );
+            }
+            catch (System.Exception ex)
+            {
+                Sentry.SentrySdk.CaptureException(ex);
+                return BadRequest(new { status = "Failed", message = ex.Message });
+            }
         }
 
         // DELETE: api/Type/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteType(Guid id)
+        [HttpDelete("{typeId}")]
+        public async Task<IActionResult> DeleteType(Guid typeId)
         {
-           
-                 try
-           {
-             var tblType = await _context.TblTypes.FindAsync(id);
-            if (tblType == null)
+            try
             {
-                return NotFound();
+                var tblType = await _context.TblTypes.FindAsync(typeId);
+                if (tblType == null)
+                {
+                    return NotFound();
+                }
+
+                _context.TblTypes.Remove(tblType);
+                await _context.SaveChangesAsync();
+
+                return Ok(new { status = "success", message = "Delete type by id successful." });
             }
-
-            _context.TblTypes.Remove(tblType);
-            await _context.SaveChangesAsync();
-
-           return Ok(
-                    new
-                    {
-                        status = "success",
-                        message = "Delete type by id successful."
-                    }
-                );
-           }
-           catch (System.Exception ex)
-           {   
-            return BadRequest(new{status="Failed",message = ex.Message});
-           }
+            catch (System.Exception ex)
+            {
+                Sentry.SentrySdk.CaptureException(ex);
+                return BadRequest(new { status = "Failed", message = ex.Message });
+            }
         }
 
-        private bool TblTypeExists(Guid id)
+        private bool IsTypeExists(Guid typeId)
         {
-            return _context.TblTypes.Any(e => e.TypeId == id);
+            return _context.TblTypes.Any(e => e.TypeId == typeId);
         }
     }
 }
