caeac1d629158bae328a7e956a4ce6abcc6e9802
jagadeshwaran-kovai-co <jagadeshwaran.rangaraj@kovai.co>
2022-03-31 14:24:23 +0530

    CheckOtpFunction

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -180,8 +180,7 @@ namespace GasB360_server.Controllers
             {
                 tblOrder.OrderOtp = OrderOtpGenerator();
                 tblOrder.EmployeeId = await AssignEmployeeId();
-                // await AddUnfilledProduct(tblOrder.FilledProductId);
-                // await RemovefilledProduct(tblOrder.FilledProductId);
+                
                 _context.TblOrders.Add(tblOrder);
 
                 await _context.SaveChangesAsync();
@@ -251,6 +250,7 @@ namespace GasB360_server.Controllers
             return employee.EmployeeId;
         }
 
+        //Function To Add A UnFilled Product After Successfull Delivery By Passing FilledProductId As Parameter
         private async Task AddUnfilledProduct(Guid? filledProductId)
         {
             var filledProduct = await _context.TblFilledProducts
@@ -265,7 +265,7 @@ namespace GasB360_server.Controllers
             _context.Entry(unfilledProduct).State = EntityState.Modified;
             await _context.SaveChangesAsync();
         }
-
+        //Function To Remove A Filled Product After Successfull Delivery By Passing FilledProductId As Parameter
         private async Task RemovefilledProduct(Guid? filledProductId)
         {
             var unfilledProduct = await _context.TblFilledProducts
@@ -280,6 +280,32 @@ namespace GasB360_server.Controllers
 
             _context.Entry(unfilledProduct).State = EntityState.Modified;
             await _context.SaveChangesAsync();
+        }
+        //Function To Check Order Delivery By OTP  By Passing OrderId And Otp As Parameter
+        private async Task<IActionResult> OrderDeliveryCheckByOtp(Guid orderId,int inputOtp){
+            var order = await _context.TblOrders.FindAsync(orderId);
+            if(order.OrderOtp==inputOtp){
+                order.OrderStatus="Delivered";
+                _context.Entry(order).State = EntityState.Modified;
+                var tblDelivery = new TblDelivery();
+                tblDelivery.OrderId=orderId;
+                _context.TblDeliveries.Add(tblDelivery);
+                await _context.SaveChangesAsync();
+                await AddUnfilledProduct(order.FilledProductId);
+                await RemovefilledProduct(order.FilledProductId);
+                return Ok(
+                    new
+                    {
+                        status = "success",
+                        message = "Delivery By Otp successfull.",
+                    }
+                );
+            }
+            else{
+                return BadRequest(new{status = "Failed",message="wrong Otp"});
+                
+            }
+
         }
     }
 }
