8783915edc7a91cd03bd4e2d17d48690decadfeb
Sabareeswaran B <sabareeswaran2510@gmail.com>
2022-04-10 20:18:03 +0530

    payment gateway error fix

--- Controllers/PaymentController.cs
+++ Controllers/PaymentController.cs
@@ -33,12 +33,18 @@ public class PaymentsController : Controller
                     .OrderBy(x => x.OrderDate)
                     .LastOrDefault()
             );
-            var lastOrderDate = (DateTime)orders!.OrderDate! - DateTime.Now;
-            if (lastOrderDate.TotalDays <= 30)
+            if (orders != null)
             {
-                return BadRequest(
-                    new { status = "falied", message = "Already ordered for this month." }
-                );
+                if (orders.OrderDate != null)
+                {
+                    var lastOrderDate = (DateTime)orders!.OrderDate! - DateTime.Now;
+                    if (lastOrderDate.TotalDays <= 30)
+                    {
+                        return BadRequest(
+                            new { status = "falied", message = "Already ordered for this month." }
+                        );
+                    }
+                }
             }
             var UnitAmount = tblOrder.OrderTotalprice.ToString() + "00";
             var filledProduct = await _context.TblFilledProducts.FindAsync(
