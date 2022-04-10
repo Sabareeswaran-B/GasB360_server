250e928147ffc01bc004c5dbcef967860367873a
Sabareeswaran B <sabareeswaran2510@gmail.com>
2022-04-10 20:32:06 +0530

    url changes

--- Controllers/PaymentController.cs
+++ Controllers/PaymentController.cs
@@ -73,8 +73,8 @@ public class PaymentsController : Controller
                 },
                 Mode = "payment",
                 SuccessUrl =
-                    $"https://gasb360.herokuapp.com/order?customerId={tblOrder.CustomerId}&filledProductId={tblOrder.FilledProductId}&addressId={tblOrder.AddressId}&orderTotalprice={tblOrder.OrderTotalprice}",
-                CancelUrl = $"https://gasb360.herokuapp.com/orders/address",
+                    $"https://gasb360.herokuapp.com/customer/order?customerId={tblOrder.CustomerId}&filledProductId={tblOrder.FilledProductId}&addressId={tblOrder.AddressId}&orderTotalprice={tblOrder.OrderTotalprice}",
+                CancelUrl = $"https://gasb360.herokuapp.com/customer/orders/address",
             };
 
             var service = new SessionService();
